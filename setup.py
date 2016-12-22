0d5cf2605ca63efbc19806a0aa315240a1535d75
Eduard Carreras <ecarreras@gisce.net>
2016-12-22 10:11:04 +0100

    Bump to v0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ooquery',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     url='https://github.com/gisce/ooquery',
     license='MIT',
