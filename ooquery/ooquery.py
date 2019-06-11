c6a6d7c4dddcf88ce550e5b0dd309101909bae4b
Gerard Dalmau <gddalmau@gmail.com>
2019-06-11 19:43:32 +0200

    NEW as_ parameter to define new column name

--- ooquery/ooquery.py
+++ ooquery/ooquery.py
@@ -17,6 +17,7 @@ class OOQuery(object):
         self._select = self.table.select()
         self.parser = self.create_parser()
         self.select_opts = {}
+        self.as_ = {}
 
     def create_parser(self):
         return Parser(self.table, self.foreign_key)
@@ -63,13 +64,14 @@ class OOQuery(object):
                 fields.append(table_field)
             else:
                 table_field = self.parser.get_table_field(self.table, field)
-                fields.append(table_field.as_(field))
+                fields.append(table_field.as_(self.as_.get(field, field)))
         return fields
 
     def select(self, fields=None, **kwargs):
         self.parser = self.create_parser()
         self._fields = fields
         self.select_opts = kwargs
+        self.as_ = kwargs.pop('as_', self.as_)
         order_by = kwargs.pop('order_by', None)
         group_by = kwargs.pop('group_by', None)
         if order_by:
