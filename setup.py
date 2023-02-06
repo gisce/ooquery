c27aaf86f888ac75aa1a2a76dafe795422253641
giscegit <devel@gisce.net>
2023-02-06 12:34:42 +0000

    Bump to v0.20.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ooquery',
-    version='0.20.1',
+    version='0.20.2',
     packages=find_packages(),
     url='https://github.com/gisce/ooquery',
     license='MIT',
